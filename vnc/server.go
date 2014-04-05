ad0458f79c893b2e4a75ed2321072986149b5ec9
graciegutman <graciegutman@gmail.com>
2014-04-05 14:42:40 -0700

    Mounds of comments

--- vnc/server.go
+++ vnc/server.go
@@ -1,7 +1,6 @@
 package vnc
 
 import (
-    "fmt"
     "log"
     "net"
 )
@@ -28,6 +27,7 @@ type ServerClientImageChans struct {
     imageChannels []chan *FBUpdateWithImage
 }
 
+// Cleans up no-longer-used client channels on the image server when a client disconnects
 func cleanUpCrew(wg *WorkerGroup, alertChan chan chan *FBUpdateWithImage) {
     for {
         select {
@@ -44,9 +44,8 @@ func Super() {
     listener, err := CreateListener()
     checkError(err)
 
-    // initialize the channel that each client thread
-    // will use to pull screenshots from the image server
-
+    // initialize the image server thread(s) (workers)
+    // and create a record of all worker channels
     workerGroup := newWorkerGroup(1)
 
     for {
@@ -54,10 +53,17 @@ func Super() {
         if err != nil {
             continue
         }
+
+//AFTER THIS POINT, CONNECTION IS ESTABLISHED WITH CLIENT
+
         imageChan := make(chan *FBUpdateWithImage)
         alertChan := make(chan chan *FBUpdateWithImage)
-        // begin buffering images
+        
+        // send a client-unique image channel to the workers
         workerGroup.BroadcastImageChans(imageChan)
+
+        // signal the workers to start taking screenshots and putting
+        // them on client image channel(s)
         workerGroup.BroadcastControlMsg(Start)
 
         // launch thread that will be responsible for cleaning up after
@@ -69,39 +75,41 @@ func Super() {
     }
 }
 
-// when you create imageChans, workerGroup, etc, create deadClients channel
-// workerGroup needs to listen on deadClients, and invoke wg.BroadcastChanToClose on each incoming message
-
 func handleClient(c chan *FBUpdateWithImage, conn net.Conn, alertChan chan chan *FBUpdateWithImage) {
-    //handshake (version exchange)
+
     defer func() {
-        // Send the "i'm dying" message on the channel
+        // Alerts cleanUpCrew that client is closing
         alertChan <- c
-        //send message to thread that handles garbage collection
         conn.Close()
     }()
  
+// HANDSHAKE WITH CLIENT BEGIN
+
+    // send and receive version info
     _, err := exchangeVersions(conn)
     if err != nil {
         return
     }
 
-    // send security level
+    // send security level supported by server
     err = sendSecurity(conn)
     if err != nil {
         return
     }
 
-    // init phase
+// HANDSHAKE WITH CLIENT END
+
+// INIT WITH CLIENT BEGIN
+
     clientInitFlag, err := receiveClientInit(conn)
-    // if client requests other connections can't join
-    // inform client that this isn't possible (at the moment)
+    // If client requests other connections can't join,
+    // disconnect; feature isn't currently supported
     if clientInitFlag != 1 || err != nil { 
-        fmt.Printf("Cannot give exclusive access to server")
+        log.Printf("Cannot give exclusive access to server \n")
         return
     }
 
-    // send serverinit msg
+    // Exchange info about what image formats are going to be used
     pixelFormat := NewPixelFormat()
     serverInitMsg := NewServerInitMsg(pixelFormat)
     err = SendServerInit(serverInitMsg, conn)
@@ -109,23 +117,26 @@ func handleClient(c chan *FBUpdateWithImage, conn net.Conn, alertChan chan chan 
         return
     }
 
+// INIT WITH CLIENT END
+
+    // Allows client to receive errors from go routines client launches
     errChan := make(chan error, 10)
 
-    // main loop starts
+
+// MAIN LOOP
+
     for {
-        //read from client
+        // Get message from client
         msg, msgNum, err := GetMsg(conn)
         if err != nil {
-            fmt.Println("err ", err, " getting message from conn")
-            fmt.Println("closing connection 1")
+            log.Printf("Error received reading message %v\n", err)
             return
         }
-        //do something with the message
+        // React to message
         MsgDispatch(conn, msgNum, c, msg, errChan)
         select {
             case err := <-errChan:
-                fmt.Println("Error Received from err chan", err)
-                fmt.Println("closing connection 2")
+                log.Printf("Error received from err chan %v\n", err)
                 return
             default:
                 continue
@@ -135,6 +146,38 @@ func handleClient(c chan *FBUpdateWithImage, conn net.Conn, alertChan chan chan 
 
 }
 
+func imageServer(errChan chan chan *FBUpdateWithImage, imageChanChan chan chan *FBUpdateWithImage, control chan ControlMessage) {
+    // Initialize a record of worker to client image channels
+    ic := newServerClientImageChans()
+    var running bool = false
+    for {
+        select {
+            case msg := <- control:
+                switch msg {
+                    case Start:
+                        running = true
+                    case Stop:
+                        running = false
+                }
+            // If super sends an image channel, append it to list of active
+            // client image channels
+            case imageChan := <- imageChanChan:
+                ic.imageChannels = append(ic.imageChannels, imageChan)
+
+            // If cleanUpCrew sends an image channel, remove client channel
+            // from list of active client image channels
+            case closedChan := <- errChan:
+                log.Printf("close channel %v\n", closedChan)
+                removeChan(closedChan, ic)
+            default:
+                if running {
+                    image := NewFBUpdateWithImage()
+                    ic.BroadcastImage(image)
+                }
+        }
+    }
+}
+
 func newServerClientImageChans() *ServerClientImageChans {
     ic := &ServerClientImageChans{
         imageChannels : make([]chan *FBUpdateWithImage, 0),
@@ -142,6 +185,7 @@ func newServerClientImageChans() *ServerClientImageChans {
     return ic
 }
 
+// initializes a worker group with numWorkers threads
 func newWorkerGroup(numWorkers int) *WorkerGroup {
     wg := &WorkerGroup{
         workerControls : make([]chan ControlMessage, numWorkers),
@@ -163,7 +207,7 @@ func newWorkerGroup(numWorkers int) *WorkerGroup {
     return wg
 }
 
-
+// Sends Start and Stop message to workers
 func (wg *WorkerGroup) BroadcastControlMsg(msg ControlMessage) {
     for _, ctrl := range wg.workerControls {
         go func(control chan ControlMessage) {
@@ -172,6 +216,7 @@ func (wg *WorkerGroup) BroadcastControlMsg(msg ControlMessage) {
     }
 }
 
+// Sends new image channel to workers
 func (wg *WorkerGroup) BroadcastImageChans(imageChan chan *FBUpdateWithImage) {
     for _, imgChanChan := range wg.workerImageChanChans {
         go func(imageChanChan chan chan *FBUpdateWithImage) {
@@ -180,6 +225,7 @@ func (wg *WorkerGroup) BroadcastImageChans(imageChan chan *FBUpdateWithImage) {
     }
 }
 
+// Sends a remove client channel message to workers
 func (wg *WorkerGroup) BroadcastChanToClose(imageChan chan *FBUpdateWithImage) {
     for _, workerErrChan := range wg.workerErrChans {
         go func(ErrChan chan chan *FBUpdateWithImage) {
@@ -188,6 +234,7 @@ func (wg *WorkerGroup) BroadcastChanToClose(imageChan chan *FBUpdateWithImage) {
     }
 }
 
+// Sends image to all clients
 func (ic *ServerClientImageChans) BroadcastImage(image *FBUpdateWithImage) {
     if len(ic.imageChannels) > 0 {
         for _, imageChan := range ic.imageChannels {
@@ -206,28 +253,4 @@ func removeChan(closedChan chan *FBUpdateWithImage, ic *ServerClientImageChans) 
     }
 }
 
-func imageServer(errChan chan chan *FBUpdateWithImage, imageChanChan chan chan *FBUpdateWithImage, control chan ControlMessage) {
-    ic := newServerClientImageChans()
-    var running bool = false
-    for {
-        select {
-            case msg := <- control:
-                switch msg {
-                    case Start:
-                        running = true
-                    case Stop:
-                        running = false
-                }
-            case imageChan := <- imageChanChan:
-                ic.imageChannels = append(ic.imageChannels, imageChan)
-            case closedChan := <- errChan:
-                fmt.Print("close channel %v", closedChan)
-                removeChan(closedChan, ic)
-            default:
-                if running {
-                    image := NewFBUpdateWithImage()
-                    ic.BroadcastImage(image)
-                }
-        }
-    }
-}
+
